4f1a72ae1eb31532626cb5cfb3f76b138c8ef84f
Ruezgar Guenduez <ruezgar.guenduez@gmail.com>
2026-02-05 12:04:16 +0100

    fixed issues with updating commands
    in discord itself, the new commands would not be shown as an option and past commands were still active
    this has been fixed
    the embeds have been roughly tested
    must still update prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <dpp/dpp.h>
 #include <thread>
 #include <fstream>
+#include <vector>
 #include "../include/json.hpp"
 
 using namespace std;
@@ -18,8 +19,8 @@ struct embedField {
 
 // prototypes
 void executeSlashCommand(string userInput, const dpp::slashcommand_t& event);
-char diceRoll(int low, int up);
-void embed(const dpp::slashcommand_t& event, string title, string desc, string imgURL="none", embedField* fields, unsigned char fieldAmount);
+string diceRoll(int low, int up);
+void embed(const dpp::slashcommand_t& event, string title, string desc, string imgURL, embedField* fields, unsigned char fieldAmount);
 dpp::embed getEmbed(string title, string desc, string imgURL, embedField* fields, unsigned char fieldAmount);
 
 // main
@@ -40,11 +41,17 @@ int main(int argc, char** argv) {
 
     bot.on_ready([&bot](const dpp::ready_t& event) {
         if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand(PING, PING_DESCRIPTION, bot.me.id));
-            bot.global_command_create(dpp::slashcommand("dice", "performs a dice roll (min 1, max 6)", bot.me.id));
-            bot.global_command_create(dpp::slashcommand("embed", "testing embeds rn", bot.me.id));
-            // bot.global_command_create(dpp::slashcommand(PING, PING_DESCRIPTION, bot.me.id));
-            // bot.global_command_create(dpp::slashcommand(PING, PING_DESCRIPTION, bot.me.id));
+
+            vector<dpp::slashcommand> commands;
+            commands.push_back(dpp::slashcommand(PING, PING_DESCRIPTION, bot.me.id));
+            commands.push_back(dpp::slashcommand("dice", "performs a dice roll (min 1, max 6)", bot.me.id));
+            commands.push_back(dpp::slashcommand("embed", "testing embeds rn", bot.me.id));
+
+            // commands.push_back(dpp::slashcommand(name, desc, bot.me.id));
+            // bot.global_command_create(dpp::slashcommand(name, desc, bot.me.id));
+
+            bot.global_bulk_command_create(commands);
+
         }
     });
 
@@ -55,38 +62,30 @@ int main(int argc, char** argv) {
 void executeSlashCommand(string userInput, const dpp::slashcommand_t& event) {
     string result;
     if (userInput == "ping") {
-        result = "Bot is ready to operate";
+        event.reply("Bot is ready to operate");
     } else if (userInput == "dice") {
-        result = diceRoll(1, 6);
+        event.reply(diceRoll(1, 6));
     } else if (userInput == "embed") {
 
         // obviously not how this will work once implemented properly
 
-        embedField field1, field2, field3;
-
-        field1.title = "title";
-        field2.title = "anothertitle";
-        field3.title = "anothertitletitle";
-
-        field1.contents = "this is some content for field1";
-        field2.contents = "this is more content for field 2";
-        field3.contents = "you didnt expect this content to be so out of line from the other two did you?";
+        embedField field1{.title = "title", .contents = "this is some content for field1", .value = true}, 
+        field2{.title = "title", .contents = "this is some content for field1", .value = true}, 
+        field3{.title = "title", .contents = "this is some content for field1", .value = true};
 
         embedField embedFields[] = {field1, field2, field3}; 
 
-        for (embedField field : embedFields)
-            field.value = true;
-
-        embed(event, "title", "description", "none", embedFields, 3);
     } else {
         event.reply("Whoops! We haven't designed an output for this command!");
     }
-    cout << "executed" << userInput << endl;
+    cout << "executed " << userInput << endl;
 }
 
-char diceRoll(int low, int up) {
+string diceRoll(int low, int up) {
     srand(time(NULL));
-    return (rand() % up + low) + '0';
+    string result; 
+    result[0] = (rand() % up + low) + '0';
+    return result;
 }
 
 void embed(const dpp::slashcommand_t& event, string title, string desc, string imgURL, embedField* fields, unsigned char fieldAmount) {
@@ -95,21 +94,20 @@ void embed(const dpp::slashcommand_t& event, string title, string desc, string i
     event.reply(msg);
 }
 
-dpp::embed getEmbed(string title, string desc, string imgURL, embedField* fields, unsigned char fieldAmount) {
-    if (imgURL == "none")
-        imgURL = "https://dpp.dev/DPP-Logo.png";
+dpp::embed getEmbed(string title, string desc, string footer, embedField* fields, unsigned char fieldAmount, string thumbnailURL, string imgURL) {
     dpp::embed embed = dpp::embed()
         .set_color(dpp::colors::sti_blue)
         .set_title(title)
         .set_description(desc)
-        .set_thumbnail(imgURL)
-        .set_image("https://dpp.dev/DPP-Logo.png")
         .set_footer(
             dpp::embed_footer()
             .set_text("Footer text")
         )
         .set_timestamp(time(0));
     
+    if (imgURL != "none") embed.set_image(imgURL);
+    if (thumbnailURL != "none") embed.set_thumbnail(thumbnailURL);
+
     for (int i = 0; i < fieldAmount; i++) {
         embed.add_field(fields[i].title, fields[i].contents, fields[i].value);
     }
